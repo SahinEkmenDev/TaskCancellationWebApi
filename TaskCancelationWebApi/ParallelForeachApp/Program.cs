7a76db1946cd9c44ca71a549ef78f72d1dfb1aec
Şahin EKMEN <sahin.ekmen6868@gmail.com>
2024-07-12 18:09:49 +0300

    TaskCancellationWebApi

---
+++ TaskCancelationWebApi/ParallelForeachApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelForeachApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string picturesPath = @"C:\\Users\\sahin\\Desktop";
+
+            var files= Directory.GetFiles(picturesPath);
+            Parallel.ForEach(files, (item) => 
+            { 
+            
+           
+            
+            });
+        }
+    }
+}
---
+++ TaskCancelationWebApi/TaskCancelationWebApi/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskCancelationWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public  class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController( ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        [HttpGet]
+        public async  Task<IActionResult> GetActionResultAsync()
+        {
+            logger.LogInformation("istek  başladı ");
+
+            Thread.Sleep(3000);
+            var mytask = new HttpClient().GetStringAsync("https://www.youtube.com");
+            var  data = mytask.Result;
+
+            logger.LogInformation("istek bitti");
+            return Ok(data);
+        }
+    }
+}
